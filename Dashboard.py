4aa8ba68627cf0e3bbf16abdafada745659d3e8a
André Luiz <aikoandreluiz@gmail.com>
2025-02-22 15:28:32 -0300

    Free Access Update

--- Dashboard.py
+++ Dashboard.py
@@ -1,40 +1,10 @@
-import os
-import dotenv
-from dotenv import load_dotenv
 import pandas as pd
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 import streamlit as st
 import plotly.express as px
 
-load_dotenv()
-folder_id = os.environ.get("FOLDER_ID")
+url = "https://docs.google.com/spreadsheets/d/12yiz9UT0UMYM1plz5XvXvq_tb5kVJiGISSyILUjn-hU/export?format=csv"
 
-# Import file with password to the service account
-filename = "google.json"
-scopes = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/drive"
-]
-
-# Link APIs with the account
-creds = ServiceAccountCredentials.from_json_keyfile_name(
-    filename=filename, scopes=scopes
-)
-
-# Connect with the account and allow acess with password
-client = gspread.authorize(creds)
-
-# Location of the archive in Google Drive
-full_sheet = client.open(
-    title="Placa de Vídeo CxB", 
-    folder_id="1HHVkEg_OQtjO5YCL7iCIH8oQ_GRdpqnF",
-)
-
-# Select page 1 and show all the table
-sheet = full_sheet.get_worksheet(0)
-data = sheet.get_all_records()
-df = pd.DataFrame(data)
+df = pd.read_csv(url)
 
 # Set vizualization as wide
 st.set_page_config(layout="wide")
